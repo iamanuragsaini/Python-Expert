ddd65340cf056a194ccf5fc4a52a507b6b6065ac
iamanuragsaini <anuragsaini2472002@gmail.com>
2022-01-05 06:25:48 +0530

    Swapping two number

---
+++ Swap.py
@@ -0,0 +1,17 @@
+
+# Swap of two number
+
+temp = 0
+a = input("Enter the number A : ")
+b = input("Enter the number B : ")
+temp = int(a)
+a = int(b)
+b = temp
+print(f"After swap number A : {a}")
+print(f"After swap number B : {b}")
+
+
+# OR
+
+'''Another method to swap'''
+a,b = b,a
