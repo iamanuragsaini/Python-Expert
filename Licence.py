8c0fc0712f3f595e1294f48177e3e8e5bdcb03f9
iamanuragsaini <anuragsaini2472002@gmail.com>
2022-01-06 07:32:36 +0530

    User is valid for driving licence or not.

---
+++ Licence.py
@@ -0,0 +1,10 @@
+#driving licence
+
+age = int(input("Enter the age "))
+if age>18:
+    print("You have valid for driving")
+elif age==18:
+    print("not decided yet")
+else:
+    print("not valid for driving")
+    
