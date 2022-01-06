d4f0422df6050537e4d30cb0ad2ebd718bea5238
iamanuragsaini <anuragsaini2472002@gmail.com>
2022-01-06 07:24:09 +0530

    Simple dictionary. Word and meaning.

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+
+# Dictionary of meaning with user input 
+
+d1 = {
+    "King":"Anurag",
+    "Queen":"No one",
+    "Country":"India",
+    "Place":"Haryana"
+}
+
+inputUser = input("Enter the value ")
+print(d1[inputUser])
