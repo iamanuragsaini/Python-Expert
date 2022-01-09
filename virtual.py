99ccf635fcb710a37aa35903fa3bcefd063d5f02
iamanuragsaini <anuragsaini2472002@gmail.com>
2022-01-09 07:17:55 +0530

    Create virtual environment and requirements.txt file.

---
+++ virtual.py
@@ -0,0 +1,10 @@
+# Virtual Environment and Requirements.txt"
+
+'''
+pip install virtualenv
+virtualenv
+virtualenv anu --- anu folder or environment name
+activate - .\anu\Scripts\activate
+pip freeze > requirements.txt
+virtualenv --system-site-package anu2
+'''
