d9affd6d70e4a676101555bff0c0030a74b07837
iamanuragsaini <anuragsaini2472002@gmail.com>
2022-01-22 08:34:55 +0530

    Use alternate constructor with the help of string.split method

---
+++ oops3.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+    
+    def printDetails(self):
+        print(f"The name is {self.name} and salary is {self.salary} and the role is {self.role}")
+
+    @classmethod
+    def from_str(cls, string):
+        param = string.split("-")
+        return cls(param[0], param[1], param[2])
+
+anurag = Student("Anurag", 80000, "Data Scientist")
+kamal = Student.from_str("Kamal-40000-CGL")
+
+kamal.printDetails()
+anurag.printDetails()
+
+    
+
+
