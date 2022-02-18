54e222f674cf8ee19c57e243377d55fd977c7766
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2022-02-18 19:16:41 +0530

    Find list length with len() method

---
+++ Problems/List/FindLength/Approach2.py
@@ -0,0 +1,9 @@
+# Second approach with len function
+# Find the length of list
+
+def interChange(List):
+    listSize = len(list)
+    return listSize
+
+list = [2,3,4,5,6,7]
+print(interChange(list))
