781d744d224c388e3f677c3a492d829a6d8b2fa6
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2022-03-09 05:16:18 +0530

    Unpack the tuple into 4 variables

---
+++ Problems/Tuple/Problem4.py
@@ -0,0 +1,15 @@
+# Unpack the tuple into 4 variables
+
+''' 
+Output:
+print(a) # should print 10
+print(b) # should print 20
+print(c) # should print 30
+print(d) # should print 40
+'''
+
+tuple1 = (10, 20, 30, 40)
+
+a,b,c,d = tuple1
+print(f"{a}\n{b}\n{c}\n{d}")
+
