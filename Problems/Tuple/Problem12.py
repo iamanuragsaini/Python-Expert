27bae8027e0d76dff9a73cabd7ed51417f9c2879
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2022-03-09 06:36:54 +0530

    How to get unique elements in nested tuple

---
+++ Problems/Tuple/Problem12.py
@@ -0,0 +1,10 @@
+# Extract Symmetric Tuples
+
+'''Input : test_list = [(6, 7), (2, 3), (7, 6)]
+Output : {(6, 7)}'''
+
+testList = [(6,7),(7,6),(2,3)]
+temp = set(testList) & {(b,a) for a,b in testList }
+print(temp)
+result = {(a,b) for a,b in temp if a<b}
+print(result)
