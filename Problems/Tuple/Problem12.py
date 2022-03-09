62a99827b5d865539044f8261f06777df3243483
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2022-03-09 06:39:46 +0530

    Extract Symmetric Tuples

--- Problems/Tuple/Problem12.py
+++ Problems/Tuple/Problem12.py
@@ -1,7 +1,9 @@
 # Extract Symmetric Tuples
 
-'''Input : test_list = [(6, 7), (2, 3), (7, 6)]
-Output : {(6, 7)}'''
+'''
+Input : test_list = [(6, 7), (2, 3), (7, 6)]
+Output : {(6, 7)}
+'''
 
 testList = [(6,7),(7,6),(2,3)]
 temp = set(testList) & {(b,a) for a,b in testList }
