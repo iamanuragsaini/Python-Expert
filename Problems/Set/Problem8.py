9870999aadd27b9b5edf4d0f90226572b6740f64
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2022-03-14 13:30:48 +0530

    Remove items from set1 that are not common to both set1 and set2

---
+++ Problems/Set/Problem8.py
@@ -0,0 +1,11 @@
+# Remove items from set1 that are not common to both set1 and set2
+
+def check_element(set1,set2):
+    set1.intersection_update(set2)
+    return set1
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+result = check_element(set1,set2)
+print(result)
