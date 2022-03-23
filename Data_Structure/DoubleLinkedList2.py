b8ebb87c214fd73a848446427ce8e8b84aa521cc
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2022-03-23 20:26:09 +0530

    Delete nodes in the double linked List

---
+++ Data_Structure/DoubleLinkedList2.py
@@ -0,0 +1,43 @@
+# Delete nodes in the double linked List
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.pref = None
+        self.nref = None
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def printElement(self):
+        if self.head is None:
+            print("Double linked list is empty!!")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data,"--> ",end=' ')
+                n = n.nref
+    
+    def add_begin(self,data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+        else:
+            newNode.nref = self.head
+            self.head.pref = newNode
+            self.head = newNode
+
+    def delete_begin(self):
+        if self.head is None:
+            print("Already empty double linked list!!")
+        else:
+            self.head = self.head.nref
+            self.head.pref = None
+
+dll1 = DoubleLinkedList()
+dll1.add_begin(5)
+dll1.add_begin(3)
+dll1.add_begin(4)
+dll1.delete_begin()
+dll1.printElement()
